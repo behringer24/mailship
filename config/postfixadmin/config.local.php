89c1188a8b892627169a1a5c5f227b60f5e80a25
behringer24 <abe@activecube.de>
2020-03-21 15:18:32 +0100

    FEAT:configure postfixadmin via env variables

--- config/postfixadmin/config.local.php
+++ config/postfixadmin/config.local.php
@@ -1,7 +1,7 @@
 <?php
-$CONF['database_type'] = 'sqlite';
-$CONF['database_name'] = '/etc/postfix/sqlite/postfixadmin.db';
+$CONF['database_type'] = getenv('POSTFIXADMIN_DB_TYPE');
+$CONF['database_name'] = getenv('POSTFIXADMIN_DB_HOST');
 
-$CONF['setup_password'] = '9aaddf10eaca196b343f17ca5de9a886:8a051a4b7876fe321d5ae47271bc17b1327ada60';
+$CONF['setup_password'] = getenv('POSTFIXADMIN_SETUP_PASSWORD');
 
-$CONF['configured'] = true;
+$CONF['configured'] = getenv('POSTFIXADMIN_SETUP_PASSWORD') != '';
